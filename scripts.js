b96f46e41b62d47fc9575e00c973f52b3b722f7c
Javier <javier.bryan@speurgroup.com>
2020-10-25 00:52:48 -0400

    added js to display a message when a user subscribes and edited the styles.css to add padding to the message

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+    const newsletter = document.getElementsByClassName('newsletter')[0];
+    var email = document.getElementById('email');
+    const btn = newsletter.getElementsByTagName('button')[0];
+    const message = newsletter.getElementsByClassName('message')[0];
+
+    var invalidMessage = `Please enter a valid email address`;
+
+    btn.onclick = ev =>{
+        ev.preventDefault();
+        message.textContent  = (!email.value || email.value == "") 
+        ? invalidMessage :  `Thank you! Your email address ${email.value} has been to our mailing list!`
+    } 
+}
